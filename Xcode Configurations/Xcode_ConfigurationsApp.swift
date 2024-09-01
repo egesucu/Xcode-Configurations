ebb6d60607bbb82a88213590b285fc5dc1a67649
Ege Sucu <iletisim@egesucu.com.tr>
2024-09-01 13:51:18 +0300

    Initial Commit

---
+++ Xcode Configurations/Xcode_ConfigurationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_ConfigurationsApp.swift
+//  Xcode Configurations
+//
+//  Created by egesucu on 1.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_ConfigurationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
